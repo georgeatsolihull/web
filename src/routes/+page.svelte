be68f487b7773f743cc628cc796c41c251a958a7
Gxorge <github@ghott.me>
2023-08-01 10:46:01 +0100

    Updated for received graded

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,12 +16,12 @@
 
         <h2>My Time at College</h2>
         <p>
-            For my further education, I attended Solihull College. Whilst there, I was a Student Rep and a Wellbeing Ambassador. My course ran for 2 years, spanning 18 units and 43 assignments. Additionally, I am proud to mention that I was awarded the Outstanding Student award at the end of both years. I finished my coursework in April 2023 with a forecasted grade of D*D*D*, the highest possible grade. 
+            For my further education, I attended Solihull College. Whilst there, I was a Student Rep and a Wellbeing Ambassador. My course ran for 2 years, spanning 18 units and 43 assignments. Additionally, I am proud to mention that I was awarded the Outstanding Student award at the end of both years. I finished my coursework in April 2023 and was awarded a grade of D*D*D* in July, the highest possible grade. 
         </p>
 
         <h2>My Work</h2>
         <p class="gweb-link-black">
-            On this site, I have <a href="/work">published all of my college assignments</a>. I have also published the source code of any programming I did on my <a href="https://github.com/georgeatsolihull">GitHub</a>. I still retain the rights to all of my work, execpt where anyone else is credited. You are free to reference my work for your assignments. You do not have permission to re-distribute or edit my work. Thanks for understanding :).
+            On this site, I have <a href="/work">published all of my college assignments</a>. I have also published the source code of any programming I did on my <a href="https://github.com/georgeatsolihull">GitHub</a>. I still retain the rights to all of my work, execpt where anyone else is credited. You are free to reference my work for your assignments. You do not have permission to re-distribute or edit my work. Thanks for understanding.
         </p>
 
     </div>
