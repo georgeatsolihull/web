70d1dfbe53d74172172fb2d622b1e26c2258fcf0
George Hotten <github@ghott.me>
2024-09-22 21:56:53 +0100

    migrate to coolify

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-node'
 import svpp from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			edge: false
-		}),
+		adapter: adapter(),
 		files: {
 			lib: "src/lib",
 			assets: "src/assets"
